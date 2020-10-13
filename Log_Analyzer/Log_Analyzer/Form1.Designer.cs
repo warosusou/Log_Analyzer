a0a4efe278953812d679b6602bd37ad357bafcbb
warosusou <warosusou@outlook.com>
2020-10-13 16:42:09 +0900

    読み込み待機画面を実装

--- Log_Analyzer/Log_Analyzer/Form1.Designer.cs
+++ Log_Analyzer/Log_Analyzer/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.DateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.UnixTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -40,7 +41,11 @@
             this.UserID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ProxyStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DocumentType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -58,11 +63,13 @@
             this.UserID,
             this.ProxyStatus,
             this.DocumentType});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 72;
             this.dataGridView1.RowTemplate.Height = 30;
-            this.dataGridView1.Size = new System.Drawing.Size(2087, 980);
+            this.dataGridView1.Size = new System.Drawing.Size(2111, 1004);
             this.dataGridView1.TabIndex = 0;
             // 
             // DateTime
@@ -70,6 +77,7 @@
             this.DateTime.HeaderText = "DateTime";
             this.DateTime.MinimumWidth = 9;
             this.DateTime.Name = "DateTime";
+            this.DateTime.ReadOnly = true;
             this.DateTime.Width = 175;
             // 
             // UnixTime
@@ -77,6 +85,7 @@
             this.UnixTime.HeaderText = "UnixTime";
             this.UnixTime.MinimumWidth = 9;
             this.UnixTime.Name = "UnixTime";
+            this.UnixTime.ReadOnly = true;
             this.UnixTime.Width = 175;
             // 
             // ProcessID
@@ -84,6 +93,7 @@
             this.ProcessID.HeaderText = "ProcessID";
             this.ProcessID.MinimumWidth = 9;
             this.ProcessID.Name = "ProcessID";
+            this.ProcessID.ReadOnly = true;
             this.ProcessID.Width = 175;
             // 
             // UserAddress
@@ -91,6 +101,7 @@
             this.UserAddress.HeaderText = "UserAddress";
             this.UserAddress.MinimumWidth = 9;
             this.UserAddress.Name = "UserAddress";
+            this.UserAddress.ReadOnly = true;
             this.UserAddress.Width = 175;
             // 
             // Action
@@ -98,6 +109,7 @@
             this.Action.HeaderText = "Action";
             this.Action.MinimumWidth = 9;
             this.Action.Name = "Action";
+            this.Action.ReadOnly = true;
             this.Action.Width = 175;
             // 
             // UnknownNum
@@ -105,6 +117,7 @@
             this.UnknownNum.HeaderText = "UnknownNum";
             this.UnknownNum.MinimumWidth = 9;
             this.UnknownNum.Name = "UnknownNum";
+            this.UnknownNum.ReadOnly = true;
             this.UnknownNum.Width = 175;
             // 
             // Status
@@ -112,6 +125,7 @@
             this.Status.HeaderText = "Status";
             this.Status.MinimumWidth = 9;
             this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
             this.Status.Width = 175;
             // 
             // URL
@@ -119,6 +133,7 @@
             this.URL.HeaderText = "URL";
             this.URL.MinimumWidth = 9;
             this.URL.Name = "URL";
+            this.URL.ReadOnly = true;
             this.URL.Width = 175;
             // 
             // UserID
@@ -126,6 +141,7 @@
             this.UserID.HeaderText = "UserID";
             this.UserID.MinimumWidth = 9;
             this.UserID.Name = "UserID";
+            this.UserID.ReadOnly = true;
             this.UserID.Width = 175;
             // 
             // ProxyStatus
@@ -133,6 +149,7 @@
             this.ProxyStatus.HeaderText = "ProxyStatus";
             this.ProxyStatus.MinimumWidth = 9;
             this.ProxyStatus.Name = "ProxyStatus";
+            this.ProxyStatus.ReadOnly = true;
             this.ProxyStatus.Width = 175;
             // 
             // DocumentType
@@ -140,18 +157,48 @@
             this.DocumentType.HeaderText = "DocumentType";
             this.DocumentType.MinimumWidth = 9;
             this.DocumentType.Name = "DocumentType";
+            this.DocumentType.ReadOnly = true;
             this.DocumentType.Width = 175;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(956, 429);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(509, 279);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(1080, 504);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "お待ちください";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2111, 1004);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -169,6 +216,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn UserID;
         private System.Windows.Forms.DataGridViewTextBoxColumn ProxyStatus;
         private System.Windows.Forms.DataGridViewTextBoxColumn DocumentType;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Log_Analyzer/Log_Analyzer/Form1.cs
+++ Log_Analyzer/Log_Analyzer/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.XPath;
 
 namespace Log_Analyzer
 {
@@ -14,30 +17,89 @@ namespace Log_Analyzer
     {
         private List<LogData> source;
         private List<LogData> showing;
+        private string waitingBaseText;
+
         public Form1()
         {
             InitializeComponent();
+            waitingBaseText = label1.Text;
             source = LogAnalyzer.Load("1_access.log");
             showing = source;
-            ShowData();
         }
 
-        private void ShowData()
+        private async void Form1_Shown(object sender, EventArgs e)
         {
-            foreach(var s in showing)
+            await ShowData();
+        }
+
+        private async Task ShowData()
+        {
+            Show_Waiting();
+            dataGridView1.SuspendLayout();
+            dataGridView1.Rows.Clear();
+            DataGridViewRow[] rows = new DataGridViewRow[showing.Count];
+            await Task.Run(()=> 
             {
-                dataGridView1.Rows.Add(s.DateTime,
-                                        s.UnixTime,
-                                        s.ProcessID,
-                                        s.UserAddress,
-                                        s.Action,
-                                        s.UnknownNum,
-                                        s.Status,
-                                        s.Url,
-                                        s.UserID,
-                                        s.ProxyStatus,
-                                        s.DocumentType);
-            }
+                for (int i = 0; i < showing.Count; i++)
+                {
+                    object[] cellValues = new object[]{ showing[i].DateTime,
+                                                    showing[i].UnixTime,
+                                                    showing[i].ProcessID,
+                                                    showing[i].UserAddress,
+                                                    showing[i].Action,
+                                                    showing[i].UnknownNum,
+                                                    showing[i].Status,
+                                                    showing[i].Url,
+                                                    showing[i].UserID,
+                                                    showing[i].ProxyStatus,
+                                                    showing[i].DocumentType };
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(dataGridView1);
+                    row.SetValues(cellValues);
+                    rows[i] = row;
+                }
+            });
+            dataGridView1.Rows.AddRange(rows);
+            dataGridView1.ResumeLayout();
+            Close_Waiting();
+        }
+
+        private void Show_Waiting()
+        {
+            pictureBox1.Visible = true;
+            label1.Visible = true;
+            dataGridView1.Dock = DockStyle.None;
+            dataGridView1.Visible = false;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.BringToFront();
+            label1.BringToFront();
+            var center = new Point((this.Width - label1.PreferredWidth) / 2, this.Height / 3);
+            label1.Location = center;
+            int lapse = 0;
+            string dot = "";
+            timer1.Interval = 250;
+            timer1.Enabled = true;
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                lapse++;
+                dot += '.';
+                if(lapse % 4 == 0)
+                {
+                    lapse = 0;
+                    dot = "";
+                }
+                label1.Text = waitingBaseText + dot;
+                timer1.Enabled = true;
+            };
+        }
+
+        private void Close_Waiting()
+        {
+            timer1.Enabled = false;
+            label1.Visible = false;
+            pictureBox1.Visible = false;
+            dataGridView1.Visible = true;
         }
     }
 }
