8f822648f4b9a754f6df5224d16b43459a1bd292
warosusou <warosusou@outlook.com>
2020-10-22 01:47:37 +0900

    タイトル画面のテキストボックスを自動的に追加、削除する機能を実装

--- Log_Analyzer/Log_Analyzer/TitleForm.Designer.cs
+++ Log_Analyzer/Log_Analyzer/TitleForm.Designer.cs
@@ -90,6 +90,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(378, 28);
             this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "t1";
             // 
             // textBox2
             // 
@@ -97,6 +98,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(445, 28);
             this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "t2";
             // 
             // groupBox2
             // 
--- Log_Analyzer/Log_Analyzer/TitleForm.cs
+++ Log_Analyzer/Log_Analyzer/TitleForm.cs
@@ -13,9 +13,11 @@ namespace Log_Analyzer
 {
     public partial class TitleForm : Form
     {
-        private TextBox[] keyBoxes;
-        private TextBox[] ignoreBoxes;
+        private List<TextBox> keyBoxes;
+        private List<TextBox> ignoreBoxes;
         public string LoadingFilePath { get; private set; } = "";
+        private const int TEXTBOX_MARGIN = 30;
+        private bool CreatingScreen = false;
 
         public TitleForm()
         {
@@ -29,64 +31,145 @@ namespace Log_Analyzer
 
         private void CreateScreen()
         {
+            CreatingScreen = true;
             button1.Enabled = false;
             button2.Enabled = false;
             button3.Enabled = false;
-            keyBoxes = new TextBox[LogAnalyzer.Keys.Count];
+            keyBoxes = new List<TextBox>();
             int x1 = textBox1.Location.X;
             int y1 = textBox1.Location.Y;
-            for (int i = 0; i < keyBoxes.Length; i++)
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < LogAnalyzer.Keys.Count; i++)
             {
-                keyBoxes[i] = new TextBox
+                var t = new TextBox
                 {
                     Text = LogAnalyzer.Keys[i],
                     Location = new Point(x1, y1),
                     Size = textBox1.Size
                 };
-                keyBoxes[i].KeyDown += (s, e) => { button2.Enabled = true; button3.Enabled = true; };
-                y1 += 30;
+                keyBoxes.Add(t);
+                t.KeyUp += stringBoxes_KeyUp;
+                y1 += TEXTBOX_MARGIN;
             }
-            groupBox1.Controls.AddRange(keyBoxes);
-            //this.Controls.AddRange(keyBoxes);
+            groupBox1.Controls.AddRange(keyBoxes.ToArray());
 
             if (LogAnalyzer.IgnoringOrder != null)
             {
-                ignoreBoxes = new TextBox[LogAnalyzer.IgnoringOrder.Count];
+                ignoreBoxes = new List<TextBox>();
                 int x2 = textBox2.Location.X;
                 int y2 = textBox2.Location.Y;
-                for (int i = 0; i < ignoreBoxes.Length; i++)
+                groupBox2.Controls.Clear();
+                for (int i = 0; i < LogAnalyzer.IgnoringOrder.Count; i++)
                 {
-                    ignoreBoxes[i] = new TextBox
+                    var t = new TextBox
                     {
                         Text = LogAnalyzer.IgnoringOrder[i].ToString(),
                         Location = new Point(x2, y2),
                         Size = textBox2.Size
                     };
-                    ignoreBoxes[i].KeyPress += intBoxes_KeyPress;
-                    y2 += 30;
+                    ignoreBoxes.Add(t);
+                    t.KeyPress += intBoxes_KeyPress;
+                    t.KeyUp += intBoxes_KeyUp;
+                    y2 += TEXTBOX_MARGIN;
                 }
-                groupBox2.Controls.AddRange(ignoreBoxes);
-                //this.Controls.AddRange(ignoreBoxes);
+                groupBox2.Controls.AddRange(ignoreBoxes.ToArray());
             }
             textBox1.Visible = false;
             textBox2.Visible = false;
 
             textBox3.Text = LogAnalyzer.UnixTimeOrder.ToString();
             textBox3.KeyPress += intBoxes_KeyPress;
-
+            stringBoxes_KeyUp(keyBoxes.Last(), new KeyEventArgs(Keys.None));
+            intBoxes_KeyUp(ignoreBoxes.Last(), new KeyEventArgs(Keys.None));
             listBox1.Items.Clear();
             foreach (var d in Directory.GetFiles("./", "*.log"))
             {
                 listBox1.Items.Add(Path.GetFileName(d));
             }
+            CreatingScreen = false;
+        }
+
+        private void stringBoxes_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (CreatingScreen)
+                e.Handled = true;
+            CheckTextBoxDeployment((TextBox)sender);
         }
 
         private void intBoxes_KeyPress(object sender, KeyPressEventArgs e)
         {
+            e.Handled = true;
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') ||(e.KeyChar >= 0 && e.KeyChar <= 31))
+                e.Handled = false;
+        }
+
+        private void intBoxes_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (CreatingScreen)
+                e.Handled = true;
+            var textBox = (TextBox)sender;
+            if (ignoreBoxes.Contains(textBox))
+                CheckTextBoxDeployment(textBox);
+        }
+
+        private void CheckTextBoxDeployment(TextBox textBox)
+        {
             button2.Enabled = true;
             button3.Enabled = true;
-            if (e.KeyChar < '0' || e.KeyChar > '9')
-                e.Handled = true;
+            List<TextBox> list;
+            GroupBox groupBox;
+            KeyEventHandler keyEventHandler;
+            KeyPressEventHandler keyPressEventHandler = null;
+
+            if (keyBoxes.Contains(textBox))
+            {
+                list = keyBoxes;
+                groupBox = groupBox1;
+                keyEventHandler = stringBoxes_KeyUp;
+            }
+            else if (ignoreBoxes.Contains(textBox))
+            {
+                list = ignoreBoxes;
+                groupBox = groupBox2;
+                keyEventHandler = intBoxes_KeyUp;
+                keyPressEventHandler = intBoxes_KeyPress;
+            }
+            else
+                return;
+            if (list.Last() != textBox && textBox.Text == "")
+                RemoveTextBox(list, groupBox, textBox);
+            else if (list.Last() == textBox && textBox.Text != "")
+                CreateTextBox(list, groupBox, textBox, keyEventHandler, keyPressEventHandler);
+        }
+
+        private void RemoveTextBox(List<TextBox> list, GroupBox groupBox, TextBox textBox)
+        {
+            groupBox.Controls.Clear();
+            int i;
+            for (i = list.Count - 1; i > list.IndexOf(textBox); i--)
+            {
+                list[i].Location = new Point(list[i - 1].Location.X, list[i - 1].Location.Y);
+            }
+            groupBox.Controls.Remove(textBox);
+            list.Remove(textBox);
+            textBox.Dispose();
+            groupBox.Controls.AddRange(list.ToArray());
+            list[i].Focus();
+        }
+
+        private void CreateTextBox(List<TextBox> list, GroupBox groupBox, TextBox textBox, KeyEventHandler keyEventHandler, KeyPressEventHandler keyPressEventHandler)
+        {
+            var t = new TextBox
+            {
+                Text = "",
+                Location = new Point(list.Last().Location.X, list.Last().Location.Y + TEXTBOX_MARGIN),
+                Size = list.Last().Size
+            };
+            t.KeyUp += keyEventHandler;
+            if (keyPressEventHandler != null)
+                t.KeyPress += keyPressEventHandler;
+            list.Add(t);
+            groupBox.Controls.Add(t);
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -107,7 +190,7 @@ namespace Log_Analyzer
         private void button2_Click(object sender, EventArgs e)
         {
             LogAnalyzer.Keys = new List<string>();
-            for (int i = 0; i < keyBoxes.Length; i++)
+            for (int i = 0; i < keyBoxes.Count; i++)
             {
                 if (keyBoxes[i].Text != "")
                     LogAnalyzer.Keys.Add(keyBoxes[i].Text);
